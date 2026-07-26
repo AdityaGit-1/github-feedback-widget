3c95fa64a4283447979f3467089f4f492f68e28f
AdityaGit-1 <tiwariaditya199153@gmail.com>
2026-07-26 12:58:22 +0530

    feat: implement feedback submission endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,25 @@
 import express from "express";
 
 const app = express();
-
 const PORT = 3000;
 
-app.get("/" , (req , res) => {
-    res.send("Hello from Express server!")
-})
+app.use(express.json());
 
-app.listen(PORT , () => {
-    console.log(`Server is running on port ${PORT}`)
-})  
+app.get("/", (req, res) => {
+    res.send("Hello from Express server!");
+});
+
+app.post("/feedback", (req, res) => {
+    const feedback = req.body;
+
+    console.log("Feedback received is", feedback);
+
+    res.status(201).json({
+        message: "Feedback received successfully",
+        data: feedback,
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
